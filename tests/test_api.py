1a3cc33ffbf7ee1c15b28ce4f58be0236ec8e2fa
Diogo Jacomini <diogo.jacomini2@gmail.com>
2025-03-02 19:11:35 -0300

    test: adicionado métodos de update e delete e organizando ordem dos testes

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,30 +6,112 @@ import json
 
 client = TestClient(app)
 
+
 def test_home():
+    """
+    Test the home endpoint.
+    Ensures that the API returns a welcome message.
+    """
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {'mensagem': 'Bem-vindo à API de Recomendação de Produtos!'}
 
 
-def test_start_products():
-    response = client.get('/produtos/')
+# region Test Users
+
+def test_user_create():
+    """
+    Test creating a new user.
+    Ensures that the API correctly registers a new user with valid data.
+    """
+    user_test: Dict[str, str] = {
+        "id": 1,
+        "cpf": "test_",
+        "nome": "Usuario de Teste",
+        "email": "test.user@email.com",
+        "telefone": "(11)999999999",
+        "endereco": "rua teste numero test",
+        "data_nascimento": "1991/02/20"
+    }
+    response = client.post('/usuarios/', json=user_test)
     assert response.status_code == 200
-    assert len(response.json()) == 12
+    assert response.json()['cpf'] == user_test['cpf']
+    assert response.json()['nome'] == user_test['nome']
+    assert response.json()['data_nascimento'] == user_test['data_nascimento']
 
 
-def test_first_product():
-    response = client.get('/produtos/1')
+def test_user_list():
+    """
+    Test retrieving all users.
+    Ensures that the API returns the list of registered users.
+    """
+    response = client.get('/usuarios/')
     assert response.status_code == 200
-    assert response.json()[0]['id'] == 1
-    assert response.json()[0]['nome'] == 'Samsung Galaxy S23'
-    assert response.json()[0]['categoria'] == 'Smartphones'
+    assert len(response.json()) == 1
 
 
-def test_create_product():
+def test_user_update():
+    """
+    Test updating an existing user.
+    Ensures that the API successfully updates a user's details.
+    """
+    updated_user: Dict[str, str] = {
+        "id": 1,
+        "cpf": "test_update",
+        "nome": "Usuario de Teste Alter",
+        "email": "test.user@email.com",
+        "telefone": "(11)999999991",
+        "endereco": "rua teste numero test",
+        "data_nascimento": "1991/02/20"
+    }
+    response = client.put('/usuarios/1', json=updated_user)
+    assert response.status_code == 200
+    assert response.json()['cpf'] == 'test_'
+    assert response.json()['nome'] == updated_user['nome']
+    assert response.json()['email'] == 'test.user@email.com'
+
+
+def test_user_get():
+    """
+    Test retrieving a specific user.
+    Ensures that the API returns correct user details for a given ID.
+    """
+    response = client.get('/usuarios/1')
+    assert response.status_code == 200
+    assert isinstance(response.json()[0]['id'], int) and response.json()[0]['id'] == 1
+    assert isinstance(response.json()[0]['cpf'], str) and response.json()[0]['cpf'] == 'test_'
+    assert isinstance(response.json()[0]['nome'], str) and response.json()[0]['nome'] == 'Usuario de Teste Alter'
+
+
+def test_user_delete():
+    """
+    Test deleting a user.
+    Ensures that the API successfully deletes a user by ID.
+    """
+    response = client.delete('/usuarios/1')
+    assert response.status_code == 200
+
+
+def test_user_get_nonexistent():
+    """
+    Test retrieving a non-existent user.
+    Ensures that the API returns a 404 status code.
+    """
+    response = client.get('/usuarios/1')
+    assert response.status_code == 404
+
+# endregion user
+
+
+# region Test Products
+def test_product_create():
+    """
+    Test creating a new product.
+    Ensures that the API correctly registers a new product with valid data.
+    """
     product_test: Dict[str, str | float | List] = {
         "id": 2148,
-        "nome": "test_",
+        "nome": "Produto de Teste",
         "descricao": "produto de teste",
         "preco": 0.99,
         "categoria": "test",
@@ -45,7 +127,11 @@ def test_create_product():
     assert len(response.json()['tags']) == 3
 
 
-def test_listar_produtos():
+def test_product_list():
+    """
+    Test retrieving all registered products.
+    Ensures that the API returns the correct number of products based on stored data.
+    """
     response = client.get('/produtos/')
     with open('app/synthetic_data/for_register_products.json', 'r', encoding='utf-8') as f:
         produtos_rw = json.load(f)
@@ -53,32 +139,52 @@ def test_listar_produtos():
     assert len(response.json()) == len(produtos_rw)
 
 
-def test_criar_usuario():
-    user_test: Dict[str, str] = {
-        "id": 2148,
-        "cpf": "test_",
-        "nome": "Usuario de Teste",
-        "email": "test.user@email.com",
-        "telefone": "(11)999999999",
-        "endereco": "rua teste numero test",
-        "data_nascimento": "1991/02/20"
-    }
-    response = client.post('/usuarios/', json=user_test)
+def test_product_update():
+    """
+    Test updating an existing product.
+    Ensures that the API successfully updates a product's details.
+    """
+    updated_product = {"id": 134,
+                       "nome": "Produto de Teste Alterado",
+                       "descricao": "produto de teste",
+                       "preco": 10.99,
+                       "categoria": "test",
+                       "tags": ["test1", "test2"]}
+
+    response = client.put('/produtos/13', json=updated_product)
     assert response.status_code == 200
-    assert response.json()['cpf'] == user_test['cpf']
-    assert response.json()['nome'] == user_test['nome']
-    assert response.json()['data_nascimento'] == user_test['data_nascimento']
+    assert response.json()['id'] == 13
+    assert response.json()['nome'] == updated_product['nome']
+    assert response.json()['preco'] == updated_product['preco']
+    assert len(response.json()['tags']) == 2
 
 
-def test_listar_users():
-    response = client.get('/usuarios/')
+def test_product_get():
+    """
+    Test retrieving all products.
+    Ensures that the API returns the expected number of products.
+    """
+    response = client.get('/produtos/13')
+    assert response.status_code == 200
+    assert isinstance(response.json()[0]['id'], int) and response.json()[0]['id'] == 13
+    assert isinstance(response.json()[0]['nome'], str) and response.json()[0]['nome'] == 'Produto de Teste Alterado'
+
+
+def test_product_delete():
+    """
+    Test deleting a product.
+    Ensures that the API successfully deletes a product by ID.
+    """
+    response = client.delete('/produtos/13')
     assert response.status_code == 200
 
 
-def test_listar_user():
-    response = client.get('/usuarios/1')
-    assert response.status_code == 200
-    assert isinstance(response.json()[0]['id'], int)
-    assert isinstance(response.json()[0]['cpf'], str)
-    assert isinstance(response.json()[0]['nome'], str)
-    assert isinstance(response.json()[0]['data_nascimento'], str)
+def test_product_get_nonexistent():
+    """
+    Test retrieving a non-existent product.
+    Ensures that the API returns a 404 status code.
+    """
+    response = client.get('/produtos/13')
+    assert response.status_code == 404
+
+# endregion Test Products
