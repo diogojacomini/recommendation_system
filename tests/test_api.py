54653bf82d454e35056028cd4e3ced0891541999
Diogo Jacomini <diogo.jacomini2@gmail.com>
2025-03-02 01:20:27 -0300

    feat(tests): adicionando testes de api - users

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,6 +2,7 @@ from typing import Dict, List
 import pytest
 from fastapi.testclient import TestClient
 from app.main import app
+import json
 
 client = TestClient(app)
 
@@ -42,3 +43,42 @@ def test_create_product():
     assert response.json()['categoria'] == product_test['categoria']
     assert response.json()['tags'] == product_test['tags']
     assert len(response.json()['tags']) == 3
+
+
+def test_listar_produtos():
+    response = client.get('/produtos/')
+    with open('app/synthetic_data/for_register_products.json', 'r', encoding='utf-8') as f:
+        produtos_rw = json.load(f)
+    assert response.status_code == 200
+    assert len(response.json()) == len(produtos_rw)
+
+
+def test_criar_usuario():
+    user_test: Dict[str, str] = {
+        "id": 2148,
+        "cpf": "test_",
+        "nome": "Usuario de Teste",
+        "email": "test.user@email.com",
+        "telefone": "(11)999999999",
+        "endereco": "rua teste numero test",
+        "data_nascimento": "1991/02/20"
+    }
+    response = client.post('/usuarios/', json=user_test)
+    assert response.status_code == 200
+    assert response.json()['cpf'] == user_test['cpf']
+    assert response.json()['nome'] == user_test['nome']
+    assert response.json()['data_nascimento'] == user_test['data_nascimento']
+
+
+def test_listar_users():
+    response = client.get('/usuarios/')
+    assert response.status_code == 200
+
+
+def test_listar_user():
+    response = client.get('/usuarios/1')
+    assert response.status_code == 200
+    assert isinstance(response.json()[0]['id'], int)
+    assert isinstance(response.json()[0]['cpf'], str)
+    assert isinstance(response.json()[0]['nome'], str)
+    assert isinstance(response.json()[0]['data_nascimento'], str)
