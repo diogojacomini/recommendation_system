ece720b8a9d3eea88caae4a39f7d7c89664d9c92
Diogo Jacomini <diogo.jacomini2@gmail.com>
2025-03-02 20:18:54 -0300

    docs: adicionando docstring para rotas de usuário e produto

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,3 +1,8 @@
+"""
+Módulo de rotas para operações relacionadas aos produtos.
+
+Este módulo define as rotas responsáveis por criar e listar, pesquisar, atualizar e excluir produtos.
+"""
 from typing import List
 from fastapi import APIRouter, HTTPException
 from app.models.products import Product, Products
@@ -8,6 +13,15 @@ products: Products = Products()
 
 @router.post("/produtos/", response_model=Product)
 def create_product(produto: Product) -> Product:
+    """
+    Rota para cadastrar produtos.
+
+    Args:
+        usuario (Product): O objeto Product a ser cadastrado.
+
+    Returns:
+        Product: Um novo produto com o ID atribuído.
+    """
     return products.set_product(produto.nome,
                                 produto.descricao,
                                 produto.preco,
@@ -17,11 +31,29 @@ def create_product(produto: Product) -> Product:
 
 @router.get("/produtos/", response_model=List[Product])
 def list_products() -> List[Product]:
+    """
+    Rota para listar todos os produtos cadastrados.
+
+    Returns:
+        List[Product]: Uma lista de objetos Product.
+    """
     return products.get_products()
 
 
 @router.get("/produtos/{id_product}", response_model=List[Product])
-def list_product(id_product: int) -> List[Product]:
+def get_product(id_product: int) -> List[Product]:
+    """
+    Rota para listar produtos pelo ID.
+
+    Args:
+        id_product (int): ID do produto a ser listado.
+
+    Returns:
+        List[Product]: Lista de produtos encontrados com o ID especificado.
+
+    Raises:
+        HTTPException: Se nenhum produto for encontrado, uma exceção HTTP 404 é levantada com a mensagem "Produto '{id_product}' não encontrado".
+    """
     produtos = products.get_product(id_product)
     if not produtos:
         raise HTTPException(status_code=404, detail=f"Produto '{id_product}' não encontrado")
@@ -30,6 +62,19 @@ def list_product(id_product: int) -> List[Product]:
 
 @router.put("/produtos/{id_product}", response_model=Product)
 def update_product(id_product: int, updated_product: Product) -> Product:
+    """
+    Rota para atualizar um produto existente.
+
+    Args:
+        id_product (int): ID do produto a ser atualizado.
+        updated_product (Product): Instância do produto com as novas informações.
+
+    Returns:
+        Product: Instância do produto atualizado.
+
+    Raises:
+        HTTPException: Se o produto não for encontrado, uma exceção HTTP 404 é levantada com a mensagem "Produto '{id_product}' não encontrado".
+    """
     product = products.update_product(id_product, updated_product)
     if not product:
         raise HTTPException(status_code=404, detail=f"Produto '{id_product}' não encontrado")
@@ -38,6 +83,18 @@ def update_product(id_product: int, updated_product: Product) -> Product:
 
 @router.delete("/produtos/{id_product}", response_model=str)
 def delete_product(id_product: int) -> str:
+    """
+    Rota para deletar um produto pelo ID.
+
+    Args:
+        id_product (int): ID do produto a ser deletado.
+
+    Returns:
+        str: Mensagem de sucesso se o produto for deletado.
+
+    Raises:
+        HTTPException: Se o produto não for encontrado, uma exceção HTTP 404 é levantada com a mensagem "Produto '{id_product}' não encontrado".
+    """
     success = products.delete_product(id_product)
     if not success:
         raise HTTPException(status_code=404, detail=f"Produto '{id_product}' não encontrado")
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,7 +1,7 @@
 """
 Módulo de rotas para operações relacionadas aos usuários.
 
-Este módulo define as rotas responsáveis por criar e listar usuários.
+Este módulo define as rotas responsáveis por criar e listar, pesquisar, atualizar e excluir usuários.
 """
 from typing import List
 from fastapi import APIRouter, HTTPException
@@ -17,7 +17,7 @@ def create_user(usuario: User) -> User:
     Rota para cadastrar usuários.
 
     Args:
-        usuario (Usuario): O objeto Usuario a ser cadastrado.
+        usuario (User): O objeto User a ser cadastrado.
 
     Returns:
         Usuario: Um novo usuário com o ID atribuído.
@@ -31,13 +31,25 @@ def list_users() -> List[User]:
     Rota para listar todos os usuários cadastrados.
 
     Returns:
-        List[Usuario]: Uma lista de objetos Usuario.
+        List[User]: Uma lista de objetos Usuario.
     """
     return users.get_users()
 
 
 @router.get("/usuarios/{id_user}", response_model=List[User])
 def get_user(id_user: int) -> List[User]:
+    """
+    Rota para listar usuários pelo ID.
+
+    Args:
+        id_user (int): ID do usuário a ser listado.
+
+    Returns:
+        List[User]: Lista de usuários encontrados com o ID especificado.
+
+    Raises:
+        HTTPException: Se nenhum usuário for encontrado, uma exceção HTTP 404 é levantada com a mensagem "Usuário '{id_user}' não encontrado".
+    """
     user = users.get_user(id_user)
     if not user:
         raise HTTPException(status_code=404, detail=f"Usuário '{id_user}' não encontrado")
@@ -46,6 +58,19 @@ def get_user(id_user: int) -> List[User]:
 
 @router.put("/usuarios/{id_user}", response_model=User)
 def update_user(id_user: int, updated_user: User) -> User:
+    """
+    Rota para atualizar um usuário existente.
+
+    Args:
+        id_user (int): ID do usuário a ser atualizado.
+        updated_user (User): Instância do usuário com as novas informações.
+
+    Returns:
+        User: Instância do usuário atualizado.
+
+    Raises:
+        HTTPException: Se o usuário não for encontrado, uma exceção HTTP 404 é levantada com a mensagem "Usuário '{id_user}' não encontrado".
+    """
     user = users.update_user(id_user, updated_user)
     if not user:
         raise HTTPException(status_code=404, detail=f"Usuário '{id_user}' não encontrado")
@@ -54,6 +79,18 @@ def update_user(id_user: int, updated_user: User) -> User:
 
 @router.delete("/usuarios/{id_user}", response_model=str)
 def delete_user(id_user: int) -> str:
+    """
+    Rota para deletar um usuário pelo ID.
+
+    Args:
+        id_user (int): ID do usuário a ser deletado.
+
+    Returns:
+        str: Mensagem de sucesso se o usuário for deletado.
+
+    Raises:
+        HTTPException: Se o usuário não for encontrado, uma exceção HTTP 404 é levantada com a mensagem "Usuário '{id_product}' não encontrado".
+    """
     success = users.delete_user(id_user)
     if not success:
         raise HTTPException(status_code=404, detail=f"Usuário '{id_user}' não encontrado")
